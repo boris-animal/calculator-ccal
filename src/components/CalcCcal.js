ea109e6d6d28d6c329d35b3c0cfa2bc1efd528ba
Aids <boris.guba@gmail.com>
2022-04-18 14:55:15 +0300

    added style

--- src/components/CalcCcal.js
+++ src/components/CalcCcal.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import "./CalcCcal.scss";
 
 const CalcCcal = () => {
   const [inputAgeValue, setInputAgeValue] = useState("");
   const [inputHeightValue, setInputHeightValue] = useState("");
   const [inputWeightValue, setInputWeightValue] = useState("");
   const [yourCcal, setYourCcal] = useState(0);
-  const [genderValue, setGenderValue] = useState("");
-  const [avctivityValue, setActivitySelect] = useState("");
+  const [genderValue, setGenderValue] = useState("male");
+  const [avctivityValue, setActivitySelect] = useState("1");
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -17,7 +18,7 @@ const CalcCcal = () => {
         5.0033 * inputHeightValue -
         6.755 * inputAgeValue;
       setYourCcal(avctivityValue * localYourCcal);
-    } else if (genderValue === "female") {
+    } else {
       const localYourCcal =
         655.0955 +
         9.5634 * inputWeightValue +
@@ -36,74 +37,122 @@ const CalcCcal = () => {
   }
 
   return (
-    <div>
-      <p>Calculator ccal</p>
-      <div>
-        <form onSubmit={onSubmit}>
-          <br />
-          Возраст, лет
-          <br />
+    <div className="calc-ccal">
+      <p className="calc-ccal__title">Calculator ccal</p>
+
+      <form onSubmit={onSubmit} className="calc-ccal__form ccal-form">
+        <div className="ccal-form__input form-input">
+          <label className="form-input__title" htmlFor="inputAgeValue">
+            Возраст, лет
+          </label>
           <input
+            className="form-input__control"
             type="number"
+            id="inputAgeValue"
             value={inputAgeValue}
+            required
+            placeholder="Введите Ваш возраст"
             onChange={(event) => setInputAgeValue(event.target.value)}
             min="18"
           />
-          <br />
-          <br />
-          Рост, в см
-          <br />
+        </div>
+        <div className="ccal-form__input form-input">
+          <label
+            className="form-input__title form-input"
+            htmlFor="inputHeightValue"
+          >
+            Рост, в см
+          </label>
           <input
             type="number"
+            className="form-input__control"
+            id="inputHeightValue"
             value={inputHeightValue}
+            required
+            placeholder="Введите Ваш рост"
             onChange={(event) => setInputHeightValue(event.target.value)}
             min="150"
           />
-          <br />
-          <br />
-          Вес, в кг
-          <br />
+        </div>
+        <div className="ccal-form__input form-input">
+          <label className="form-input__title" htmlFor="inputWeightValue">
+            Вес, в кг
+          </label>
           <input
             type="number"
+            className="form-input__control"
+            id="inputWeightValue"
             value={inputWeightValue}
+            required
+            placeholder="Введите Ваш вес"
             onChange={(event) => setInputWeightValue(event.target.value)}
             min="35"
           />
-          <br />
-          <br />
-          Пол <br />
-          <input
-            type="radio"
-            id="genderMale"
-            onChange={onChangeGenderValue}
-            value="male"
-            checked={genderValue === "male" ? true : false}
-          />
-          <label for="genderMale">Мужской</label>
-          <input
-            type="radio"
-            id="genderMale"
-            onChange={onChangeGenderValue}
-            value="female"
-            checked={genderValue === "female" ? true : false}
-          />
-          <label for="genderMale">Женский</label>
-          <br />
-          <br />
-          Степень физической активности <br />
-          <select value={avctivityValue} onChange={changeActivitySelect}>
-            <option value="1.2">Нет активности</option>
-            <option value="1.375">Умеренная активность</option>
-            <option value="1.55">Средняя активность</option>
-            <option value="1.725">Высокая активность</option>
+        </div>
+        <div className="ccal-form__input form-input">
+          <p className="form-input__title">Пол</p>
+          <div className="form-input__gender-control gender-control">
+            <div className="gender-control__item gender-item">
+              <input
+                type="radio"
+                className="gender-item__value"
+                id="genderMale"
+                onChange={onChangeGenderValue}
+                value="male"
+                checked={genderValue === "male" ? true : false}
+              />
+              <label className="gender-item__title" htmlFor="genderMale">
+                Мужской
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                className="gender-item__value"
+                id="genderFemale"
+                onChange={onChangeGenderValue}
+                value="female"
+                checked={genderValue === "female" ? true : false}
+              />
+              <label className="gender-item__title" htmlFor="genderFemale">
+                Женский
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="ccal-form__input form-input">
+          <label htmlFor="avctivityValueTitle" className="form-input__title">
+            Степень физической активности
+          </label>
+          <select
+            id="avctivityValue"
+            className="form-input__control"
+            value={avctivityValue}
+            onChange={changeActivitySelect}
+          >
+            <option value="1" defaultValue>
+              Базальный метаболизм
+            </option>
+            <option value="1.2">Сидячий образ жизни</option>
+            <option value="1.375">
+              Умеренная активность(1-2 тренировки в неделю)
+            </option>
+            <option value="1.55">
+              Средняя активность(3-5 тренировки в неделю)
+            </option>
+            <option value="1.725">
+              Высокая активность(5-7 тренировки в неделю)
+            </option>
             <option value="1.9">Спортсмены</option>
           </select>
-          <br />
-          <br />
-          <button>Рассчитать</button>
-          <p>Ваша суточная норма калорий: {Math.ceil(yourCcal)}</p>
-        </form>
-      </div>
+        </div>
+        <button className="ccal-form__button">Рассчитать</button>
+      </form>
+      {yourCcal !== 0 && (
+        <p className="calc-ccal__answer">
+          Ваша суточная норма калорий: {Math.ceil(yourCcal)}
+        </p>
+      )}
     </div>
   );
 };
